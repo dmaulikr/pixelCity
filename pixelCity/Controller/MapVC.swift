9946128f394fcd3970a6faa59a879cad5e7dbbcd
Christian Deciga <christian@everipedia.com>
2017-08-20 23:43:45 -0700

    changed original VC to MapVC

--- pixelCity/Controller/MapVC.swift
+++ pixelCity/Controller/MapVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapVC.swift
 //  pixelCity
 //
 //  Created by everipedia_iMac on 8/20/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
